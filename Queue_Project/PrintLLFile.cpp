0c6cfa979bc93a546f8dc6a7cac248a6d9f926ad
fhawas87 <jakubwozniak775@gmail.com>
2025-05-24 10:26:51 +0200

    Add files via upload

---
+++ Queue_Project/PrintLLFile.cpp
@@ -0,0 +1,24 @@
+#include <fstream>
+#include <string>
+#include <iostream>
+
+void PrintLLFile (const std::string &filename) {
+
+    std::ifstream file(filename);
+
+    if (!file) {std::cerr << "Error while opening file !\n"; return;}
+
+    std::string line;
+    int LineCounter = 0;
+
+    while (std::getline(file, line) && LineCounter < 1700) { // Set a limit and a counter since when I was printing out whole LLVM IR the output 
+                                                             // Of main program was cutted of since space required for this ll file is to big
+
+        std::cout << LineCounter << ". " << line << std::endl;
+        LineCounter++;
+
+    }
+
+    file.close();
+}
+
